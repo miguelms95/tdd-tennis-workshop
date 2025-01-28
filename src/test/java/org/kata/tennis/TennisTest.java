9ac38d713b03651bdc565c930b7b3253ccdc6a16
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-01-28 19:56:40 +0100

    chore: base project

---
+++ src/test/java/org/kata/tennis/TennisTest.java
@@ -0,0 +1,15 @@
+package org.kata.tennis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class TennisTest {
+
+    @Test
+    void sampleTrue() {
+        assertTrue(true);
+    }
+
+}
